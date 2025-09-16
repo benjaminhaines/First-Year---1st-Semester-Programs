45795d75ff110099930085a7f315b357a4bf9e61
Benjamin Haines <ben.haines@tcu.edu>
2025-09-16 18:41:13 -0500

    Add files via upload

--- Lab0/Lab0.java
+++ Lab0/Lab0.java
@@ -1,1 +1,7 @@
-
+// Benjamin Haines COSC 10403, You Need to Run, Write, and Compile a program that prints out Hello Wolrd to the User, can't let the program fail to run the code
+public class Lab0
+{
+    public static void main(String[] args) {
+    System.out.println("Hello World!");
+    }
+}
