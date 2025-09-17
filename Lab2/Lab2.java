4ca235b2cfc1a48427a89f5370bc2e7752c22c62
Benjamin Haines <ben.haines@tcu.edu>
2025-09-16 20:59:39 -0500

    Add files via upload

--- Lab2/Lab2.java
+++ Lab2/Lab2.java
@@ -1,1 +1,16 @@
+import java.util.*;
+public class Lab2
+{
+    public static void main(String[] args) {
+        double firstNum, secondNum, thirdNum;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter your first number: ");
+        firstNum = keyboard.nextDouble();
+        System.out.println("Enter your second number: ");
+        secondNum = keyboard.nextDouble();
+        System.out.println("Enter your third number: ");
+        thirdNum = keyboard.nextDouble();
+        System.out.println("The sum and average of your three numbers: " + (firstNum + secondNum + thirdNum)/3);
+    }
 
+}
