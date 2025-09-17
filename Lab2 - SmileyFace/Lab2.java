91876c1cba3d482e43104ef3b89844e07bff2c9b
Benjamin Haines <ben.haines@tcu.edu>
2025-09-16 21:33:12 -0500

    Add files via upload

---
+++ Lab2 - SmileyFace/Lab2.java
@@ -0,0 +1,13 @@
+public class Lab2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("\t****************");
+        System.out.println("\t\b*\t\t*");
+        System.out.println("\t\b*\t\b0 0\t*");
+        System.out.println("\t\b*\t\b ^\t*");
+        System.out.println("\t\b*\t\b\b\\___/\t*");
+        System.out.println("\t****************");
+
+    }
+}
